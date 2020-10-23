ea21301b21df3eb5b4d83d4c42a26d64a96fea9c
Jessie Schweigert <jessie@schweigert.dev>
2020-10-22 23:27:50 -0500

    Add overrideable Write method to packets

--- src/ProjectBedrock/Minecraft/Packets.cs
+++ src/ProjectBedrock/Minecraft/Packets.cs
@@ -1,12 +1,25 @@
-﻿namespace ProjectBedrock.Minecraft.Packets
+﻿using System;
+using System.Buffers;
+
+namespace ProjectBedrock.Minecraft.Packets
 {
-    public abstract record Packet();
+    public abstract record Packet()
+    {
+        public virtual void Write(IBufferWriter<byte> writer) =>
+            throw new NotImplementedException("Writing this packet type is not intended.");
+    }
 
     public record Ping(long Payload) : Packet();
     public record Pong(long Payload) : Packet();
 
     public record Request() : Packet();
-    public record Response(string JsonResponse) : Packet();
+    public record Response(string JsonResponse) : Packet()
+    {
+        public override void Write(IBufferWriter<byte> writer)
+        {
+            base.Write(writer);
+        }
+    }
 
     public record Handshake(int ProtocolVersion, string Address, ushort Port, int State) : Packet();
 }
